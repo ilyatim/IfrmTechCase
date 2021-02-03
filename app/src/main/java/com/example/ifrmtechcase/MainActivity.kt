e91d7958c30a2f1f2d3ce9ed9b0f36911c12142e
ilyatim <31735653+ilyatim@users.noreply.github.com>
2021-02-03 17:16:24 +0300

    temp

--- app/src/main/java/com/example/ifrmtechcase/MainActivity.kt
+++ app/src/main/java/com/example/ifrmtechcase/MainActivity.kt
@@ -2,18 +2,20 @@ package com.example.ifrmtechcase
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ifrmtechcase.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(),  {
 
     private final val REQUEST_CODE = 1
 
@@ -31,22 +33,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding.recyclerview.adapter = adapter
         binding.recyclerview.layoutManager = LinearLayoutManager(this)
-        if (!hasPhoneContactsPermission(Manifest.permission.READ_CONTACTS)) {
-            requestPermissions(
-                arrayOf(
-                    Manifest.permission.READ_CONTACTS
-                ), REQUEST_CODE
-            )
-        }
-        else {
-            viewModel.initContacts()
-        }
+
+        checkContactPermission()
+
         viewModel.contacts.observe(this, Observer {
             adapter.submitList(it)
         })
+
         setContentView(binding.root)
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val searchItem: MenuItem = menu.findItem(R.id.action_search)
+        val searchView: SearchView = searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return true
+    }
 
+    fun onQueryTextChange(query: String?): Boolean {
+        return false
+    }
+
+    fun onQueryTextSubmit(query: String?): Boolean {
+        return false
+    }
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -58,6 +68,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkContactPermission() {
+        if (!hasPhoneContactsPermission(Manifest.permission.READ_CONTACTS)) {
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission.READ_CONTACTS
+                ), REQUEST_CODE
+            )
+        }
+        else {
+            viewModel.initContacts()
+        }
+    }
     private fun hasPhoneContactsPermission(permission: String): Boolean {
         val hasPermission: Int = ContextCompat.checkSelfPermission(
             applicationContext,
