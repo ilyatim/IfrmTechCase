79cb93b20bdc306e0cfad3f5c8efe7530cc9dace
ilyatim <31735653+ilyatim@users.noreply.github.com>
2021-02-03 16:14:17 +0300

    feat: recycler view + permission

--- app/src/main/java/com/example/ifrmtechcase/MainActivity.kt
+++ app/src/main/java/com/example/ifrmtechcase/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.ifrmtechcase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ifrmtechcase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private final val REQUEST_CODE = 1
+
+    private val viewModel: ViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val adapter = Adapter(
+            LayoutInflater.from(this),
+            AdapterDiffUtil()
+        )
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.recyclerview.adapter = adapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(this)
+        if (!hasPhoneContactsPermission(Manifest.permission.READ_CONTACTS)) {
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission.READ_CONTACTS
+                ), REQUEST_CODE
+            )
+        }
+        else {
+            viewModel.initContacts()
+        }
+        viewModel.contacts.observe(this, Observer {
+            adapter.submitList(it)
+        })
+        setContentView(binding.root)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> viewModel.initContacts()
+        }
+    }
+
+    private fun hasPhoneContactsPermission(permission: String): Boolean {
+        val hasPermission: Int = ContextCompat.checkSelfPermission(
+            applicationContext,
+            permission
+        )
+        return hasPermission == PackageManager.PERMISSION_GRANTED
     }
 }
