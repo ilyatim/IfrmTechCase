91be771c8352204648e410e4301df0eb9c207269
ilyatim <31735653+ilyatim@users.noreply.github.com>
2021-02-03 14:39:07 +0300

    feat: dep + contact

---
+++ app/src/main/java/com/example/ifrmtechcase/Contact.kt
@@ -0,0 +1,7 @@
+package com.example.ifrmtechcase
+
+data class Contact(
+    val name: String,
+    val number: String,
+    val photoUri: String,
+)
---
+++ app/src/main/java/com/example/ifrmtechcase/ContactRepo.kt
@@ -0,0 +1,47 @@
+package com.example.ifrmtechcase
+
+import android.content.Context
+import android.database.Cursor
+import android.provider.ContactsContract
+
+class ContactRepo(private val context: Context) {
+
+    private val contacts: MutableList<Contact> = mutableListOf()
+
+    fun getContacts(): List<Contact> {
+
+        //clearContacts()
+
+        val cursor: Cursor? = context.contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null,
+                null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+            )
+
+        cursor?.use {
+            if (cursor.count > 0) {
+                while (cursor.moveToNext()) {
+                    val name =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                    val number =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    val photoUri =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI))
+                    contacts.add(Contact(
+                        name,
+                        number,
+                        photoUri
+                    ))
+                }
+            }
+        }
+
+        return contacts
+    }
+
+    private fun clearContacts() {
+        contacts.clear()
+    }
+}
